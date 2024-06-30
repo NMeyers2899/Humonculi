c705dc77f65b7f64e541ebe73b9a2394c8eef3e9
Nicholas <nmeyers2899@gmail.com>
2024-06-30 12:57:02 -0700

    Attempted to Implement an Item Pick up System

--- Assets/Scripts/Gameplay/PlayerInventoryBehavior.cs
+++ Assets/Scripts/Gameplay/PlayerInventoryBehavior.cs
@@ -1,20 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class PlayerInventoryBehavior : MonoBehaviour
 {
-    private Item[] _inventory;
+    [Tooltip("The inventory for the player.")]
+    [SerializeField]
+    private Item[] _inventory = new Item[0];
 
     public Item[] Inventory { get { return _inventory; } }
 
+    [SerializeField]
+    [Tooltip("How far away an object can be picked up from by the player.")]
+    private float _pickUpRange = 10;
+
+    [Tooltip("The object that will be hit with a ray.")]
+    private RaycastHit _hit;
+
     private void Update()
     {
-        
-    }
+        if(Input.GetKey(KeyCode.E))
+        {
+            for(int i = 0; i < Inventory.Length; i++) 
+                print(_inventory[i].ItemName); 
+        }
 
-    private void OnMouseDown()
-    {
+        // Check to see what the player is directly looking at
+        Ray ray = Camera.main.ScreenPointToRay(transform.forward);
         
+        if(Physics.Raycast(ray, out _hit) && Vector3.Distance(transform.position, _hit.transform.position) <= _pickUpRange)
+        {
+            ItemPickupBehavior newItem = _hit.transform.GetComponent<ItemPickupBehavior>();
+            
+            // If the player left clicks...
+            if (Input.GetMouseButtonDown(0) && newItem)
+            {
+                _inventory[_inventory.Length] = newItem.ItemData;
+                newItem.PickedUp();
+            }
+                
+        }
     }
 }
