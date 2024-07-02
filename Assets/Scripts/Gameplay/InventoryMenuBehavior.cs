5abe9dd90211c84c6fd924b8d68940d17f81d94d
Nicholas <nmeyers2899@gmail.com>
2024-07-02 09:58:43 -0700

    Item Icons Change as the Items in the Inventory Change

--- Assets/Scripts/Gameplay/InventoryMenuBehavior.cs
+++ Assets/Scripts/Gameplay/InventoryMenuBehavior.cs
@@ -13,19 +13,34 @@ public class InventoryMenuBehavior : MonoBehaviour
     [SerializeField]
     private Button[] _itemButtons = new Button[20];
 
+    [Tooltip("The icon that will be used for empty item slots.")]
+    [SerializeField]
+    private Sprite _emptyIcon;
+
     private void OnEnable()
     {
+        // Check through each item in the player's inventory.
         for (int i = 0; i < _playerInventory.Inventory.Length; i++)
         {
+            // Represents the text and icons of the inventory slots.
+            Text text = _itemButtons[i].GetComponentInChildren<Text>(); 
+            Image icon = _itemButtons[i].GetComponentInChildren<Image>();
+
+            // If spot "i" in the inventory has an item...
             if(_playerInventory.Inventory[i])
             {
-                _itemButtons[i].GetComponentInChildren<Text>().text = _playerInventory.Inventory[i].ItemName;
+                // ...set the text to match the item's name and the icon to match its given sprite.
+                text.text = _playerInventory.Inventory[i].ItemName;
 
-                _itemButtons[i].GetComponentInChildren<Image>().sprite = _playerInventory.Inventory[i].Icon;
+                icon.sprite = _playerInventory.Inventory[i].Icon;
             }
+            // If the spot doesn't have an item...
             else
             {
-                _itemButtons[i].GetComponentInChildren<Text>().text = "";
+                // ...set the name to be empty and the icon to be the empty one. c
+                text.text = "";
+
+                icon.sprite = _emptyIcon;
             }
         } 
     }
